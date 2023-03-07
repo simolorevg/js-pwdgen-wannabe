11664ee6aa398796de0da936a1058888eb6631ff
simolorevg <simoneviaggi@libero.it>
2023-03-07 15:00:00 +0100

    Made the script, now I work to the style of the page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert("Ciao Mondo");
+const userName = prompt('Inserisci il tuo nome');
+console.log(userName);
+const userSurname = prompt('Inserisci il tuo cognome');
+console.log(userSurname);
+const userColour = prompt ('Inserisci il tuo colore preferito');
+console.log(userColour);
+const passwordGenerated = userName + userSurname + userColour + 21;
+console.log(passwordGenerated);
+document.getElementById('password-result').innerHTML = passwordGenerated;
